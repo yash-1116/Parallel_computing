34f6308601ba09c17f932064bdf5fd6ff225e9d2
yash-1116 <yash.23cs188@sode-edu.in>
2026-08-05 09:24:23 +0530

    Create LAB2.c

---
+++ LAB2.c
@@ -0,0 +1,19 @@
+/*openmp program that divides the iterations into chunks containing 2 iterations, respectively. its input should be
+number of iterations and its output should be which iterations of parallelized for loop are executed by which thread*/
+#include<stdio.h>
+#include<omp.h>
+int main()
+{
+    int num_iterations;
+    printf("Enter the number of iterations");
+    scanf("%d", &num_iterations);
+    #pragma omp parallel 
+    {
+        #pragma omp for schedule(static,2)
+        for(int i=0;i<num_iterations;i++)
+        {
+            printf("Thread %d is executing iteration %d\n", omp_get_thread_num(), i);
+        }
+    }
+    return 0;
+}
